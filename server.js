ebf874c53e3fb804451a2546aabd95e972c2791c
Ashar Ali <ashar.alia1999@gmail.com>
2021-06-17 23:46:07 +0500

    commit

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const DB = process.env.MONGO_STRING.replace(
   "<PASSWORD>",
   process.env.MONGO_PASSWORD
 );
-console.log(DB);
 mongoose
   .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((connection) => {
