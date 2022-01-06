c184f02cf172629da896c9cb092a05158d1fb4b8
asharali95 <ashar.alia1999@gmail.com>
2022-01-06 18:09:12 +0500

    stripe webhook test 01

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const orderRouter = require("./routes/orderRoutes");
 const cors = require("cors");
 const rateLimit = require("express-rate-limit"); //to resist brute force attack
 const mongoSanitize = require("express-mongo-sanitize"); // to resist noSQLQueryInjection
+const bodyParser = require("body-parser");
 var xss = require("xss-clean"); // for XSS attack (remove script tags)
+const { stripeWebHook } = require("./controllers/orderController");
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, //
@@ -25,7 +27,12 @@ app.use(limiter);
 app.use(express.json());
 app.use(mongoSanitize());
 app.use(xss());
-
+// stripe
+app.post(
+  "/stripe-webhook",
+  bodyParser.raw({ type: "application/json" }),
+  stripeWebHook
+);
 //routers
 app.use("/api/v1/arts", artRouter);
 app.use("/api/v1/auth", authRouter);
--- controllers/orderController.js
+++ controllers/orderController.js
@@ -43,3 +43,39 @@ exports.generateCheckoutSession = async (req, res) => {
     res.status(400).json({ message: error });
   }
 };
+
+exports.stripeWebHook = (request, response) => {
+  const sig = request.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      request.body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+  }
+  if (event.type === "checkout.session.completed") {
+    console.log("session checkout completed");
+  }
+  // Handle the event
+  // switch (event.type) {
+  //   case "payment_intent.succeeded":
+  //     const paymentIntent = event.data.object;
+  //     console.log("PaymentIntent was successful!");
+  //     break;
+  //   case "payment_method.attached":
+  //     const paymentMethod = event.data.object;
+  //     console.log("PaymentMethod was attached to a Customer!");
+  //     break;
+  //   // ... handle other event types
+  //   default:
+  //     console.log(`Unhandled event type ${event.type}`);
+  // }
+
+  // Return a response to acknowledge receipt of the event
+  response.json({ received: true });
+};
