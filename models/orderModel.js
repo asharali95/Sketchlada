6e6a2ea4a6276cd0ba2c3b229827c5b90c24f993
asharali95 <ashar.alia1999@gmail.com>
2022-01-07 16:21:51 +0500

    timestamp added in order model

--- models/orderModel.js
+++ models/orderModel.js
@@ -3,11 +3,16 @@ const {
   Schema: { ObjectId },
 } = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  art: { type: ObjectId, ref: "art", required: true },
-  buyer: { type: ObjectId, ref: "buyer", required: true },
-  artist: { type: ObjectId, ref: "artist", required: true },
-});
+const orderSchema = new mongoose.Schema(
+  {
+    art: { type: ObjectId, ref: "art", required: true },
+    buyer: { type: ObjectId, ref: "buyer", required: true },
+    artist: { type: ObjectId, ref: "artist", required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 var Order = new mongoose.model("Order", orderSchema);
 
